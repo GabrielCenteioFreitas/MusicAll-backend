b596b1c95638aa8ed4bc1024596a5f9d84fbb14d
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-06-04 19:37:19 -0300

    feat: add Auth route

---
+++ src/routes/auth.ts
@@ -0,0 +1,114 @@
+import axios from "axios";
+import { FastifyInstance } from "fastify";
+import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+
+export async function authRoutes(app: FastifyInstance) {
+  app.post('/register', async (request) => {
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body)
+
+    const redirectURI = process.env.NODE_ENV === 'production'
+      ? 'https://musical-project.vercel.app/api/auth/callback'
+      : 'http://localhost:3000/api/auth/callback'
+
+    console.log(redirectURI)
+
+    const accessTokenResponse = await axios.post(
+      'https://oauth2.googleapis.com/token',
+      null, //body
+      {
+        params: {
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET,
+          code,
+          redirect_uri: redirectURI,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          Accept: 'application/json'
+        }
+      } //config
+    ).catch(error => {
+      console.log(error)
+    })
+
+    const { access_token } = accessTokenResponse?.data
+
+    const userResponse = await axios.get(
+      'https://people.googleapis.com/v1/people/me',
+      {
+        params: {
+          personFields: 'names,photos,metadata'
+        },
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        }
+      }
+    ).catch(error => {
+      console.log(error)
+    })
+
+    const userSchema = z.object({
+      metadata: z.object({
+        sources: z.array(
+          z.object({
+            id: z.string(),
+          })
+        ),
+      }),
+      names: z.array(
+        z.object({
+          displayName: z.string(),
+        })
+      ),
+      photos: z.array(
+        z.object({
+          url: z.string().url(),
+        })
+      )
+    });
+
+    const userInfo = userSchema.parse(userResponse?.data)
+
+    let user = undefined
+
+    try {
+      user = await prisma.user.findUnique({
+        where: {
+          googleId: userInfo.metadata.sources[0].id
+        }
+      });
+    } catch (error) {
+      console.log(error)
+    }
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.metadata.sources[0].id,
+          name: userInfo.names[0].displayName,
+          avatarUrl: userInfo.photos[0].url
+        }
+      })
+    }
+
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl
+      }, // no primeiro {} os dados q queremos q sejam públicos
+      {
+        sub: user.id, // sub vem de subject e é o que identifica o usuário, no caso o id
+        expiresIn: '30 days', // em quanto tempo o token vai expirar
+      },
+    )
+
+    return {
+      token
+    }
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
+
+app.register(cors,{
+	origin: true,
+})
+
+app.register(jwt, {
+	 secret: 'musicall-backend-gabrielcenteiofreitas',
+	 //uma forma de diferenciar os jwts gerados desse backend de outros backends, pode colocar QUALQUER coisa
+})
+
+app.register(authRoutes)
 
 app
 	.listen({
