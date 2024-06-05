593d6cde531d7558d882aae461965d2862c1b576
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-06-05 13:47:53 -0300

    chore: fix eslint issues

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,89 +1,91 @@
 import axios from "axios";
 import { FastifyInstance } from "fastify";
-import { z } from 'zod'
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('/register', async (request) => {
+  app.post("/register", async (request) => {
     const bodySchema = z.object({
       code: z.string(),
-    })
+    });
 
-    const { code } = bodySchema.parse(request.body)
+    const { code } = bodySchema.parse(request.body);
 
-    const redirectURI = process.env.NODE_ENV === 'production'
-      ? 'https://musicall-project.vercel.app/api/auth/callback'
-      : 'http://localhost:3000/api/auth/callback'
+    const redirectURI =
+      process.env.NODE_ENV === "production"
+        ? "https://musicall-project.vercel.app/api/auth/callback"
+        : "http://localhost:3000/api/auth/callback";
 
-    console.log(redirectURI)
+    console.log(redirectURI);
 
-    const accessTokenResponse = await axios.post(
-      'https://oauth2.googleapis.com/token',
-      null, //body
-      {
+    const accessTokenResponse = await axios
+      .post(
+        "https://oauth2.googleapis.com/token",
+        null, // body
+        {
+          params: {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            code,
+            redirect_uri: redirectURI,
+            grant_type: "authorization_code",
+          },
+          headers: {
+            Accept: "application/json",
+          },
+        }, // config
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+
+    const { access_token: acessToken } = accessTokenResponse?.data;
+
+    const userResponse = await axios
+      .get("https://people.googleapis.com/v1/people/me", {
         params: {
-          client_id: process.env.GOOGLE_CLIENT_ID,
-          client_secret: process.env.GOOGLE_CLIENT_SECRET,
-          code,
-          redirect_uri: redirectURI,
-          grant_type: 'authorization_code'
+          personFields: "names,photos,metadata",
         },
         headers: {
-          Accept: 'application/json'
-        }
-      } //config
-    ).catch(error => {
-      console.log(error)
-    })
-
-    const { access_token } = accessTokenResponse?.data
-
-    const userResponse = await axios.get(
-      'https://people.googleapis.com/v1/people/me',
-      {
-        params: {
-          personFields: 'names,photos,metadata'
+          Authorization: `Bearer ${acessToken}`,
         },
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        }
-      }
-    ).catch(error => {
-      console.log(error)
-    })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
     const userSchema = z.object({
       metadata: z.object({
         sources: z.array(
           z.object({
             id: z.string(),
-          })
+          }),
         ),
       }),
       names: z.array(
         z.object({
           displayName: z.string(),
-        })
+        }),
       ),
       photos: z.array(
         z.object({
           url: z.string().url(),
-        })
-      )
+        }),
+      ),
     });
 
-    const userInfo = userSchema.parse(userResponse?.data)
+    const userInfo = userSchema.parse(userResponse?.data);
 
-    let user = undefined
+    let user;
 
     try {
       user = await prisma.user.findUnique({
         where: {
-          googleId: userInfo.metadata.sources[0].id
-        }
+          googleId: userInfo.metadata.sources[0].id,
+        },
       });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
     if (!user) {
@@ -91,24 +93,24 @@ export async function authRoutes(app: FastifyInstance) {
         data: {
           googleId: userInfo.metadata.sources[0].id,
           name: userInfo.names[0].displayName,
-          avatarUrl: userInfo.photos[0].url
-        }
-      })
+          avatarUrl: userInfo.photos[0].url,
+        },
+      });
     }
 
     const token = app.jwt.sign(
       {
         name: user.name,
-        avatarUrl: user.avatarUrl
+        avatarUrl: user.avatarUrl,
       }, // no primeiro {} os dados q queremos q sejam públicos
       {
         sub: user.id, // sub vem de subject e é o que identifica o usuário, no caso o id
-        expiresIn: '30 days', // em quanto tempo o token vai expirar
+        expiresIn: "30 days", // em quanto tempo o token vai expirar
       },
-    )
+    );
 
     return {
-      token
-    }
-  })
+      token,
+    };
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,27 @@
-import 'dotenv/config'
+import "dotenv/config";
 
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import { authRoutes } from './routes/auth'
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import fastify from "fastify";
+import { authRoutes } from "./routes/auth";
 
-const app = fastify()
+const app = fastify();
 
-app.register(cors,{
-	origin: true,
-})
+app.register(cors, {
+  origin: true,
+});
 
 app.register(jwt, {
-	 secret: 'musicall-backend-gabrielcenteiofreitas',
-	 //uma forma de diferenciar os jwts gerados desse backend de outros backends, pode colocar QUALQUER coisa
-})
+  secret: "musicall-backend-gabrielcenteiofreitas",
+  // uma forma de diferenciar os jwts gerados desse backend de outros backends, pode colocar QUALQUER coisa
+});
 
-app.register(authRoutes)
+app.register(authRoutes);
 
 app
-	.listen({
-		port: process.env.PORT ? Number(process.env.PORT) : 3333,
-	}).then(() => {
-		console.log('🚀 HTTP server running')
-	})
+  .listen({
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running");
+  });
